42a4cff5e7d8ed34980bcb7e95979e64b7ce5137
shrey_akan <shereyansjain0904@gmail.com>
2023-11-08 01:10:25 -0800

    upload image

--- src/main/java/com/demo/oragejobsite/controller/FileUploadController.java
+++ src/main/java/com/demo/oragejobsite/controller/FileUploadController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
