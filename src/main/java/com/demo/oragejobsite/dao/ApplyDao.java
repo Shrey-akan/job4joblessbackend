3b5891c9ca976592b7e2c834f39afd6e53ab9ecc
shrey_akan <shereyansjain0904@gmail.com>
2024-01-02 22:42:59 -0800

    updating api for

--- src/main/java/com/demo/oragejobsite/dao/ApplyDao.java
+++ src/main/java/com/demo/oragejobsite/dao/ApplyDao.java
@@ -2,6 +2,8 @@ package com.demo.oragejobsite.dao;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,8 @@ import com.demo.oragejobsite.entity.ApplyJob;
 public interface ApplyDao extends JpaRepository<ApplyJob, String>{
 	 // Define a custom method to find an ApplyJob entity by juid
     ApplyJob findByJuid(String juid);
+
+	List<ApplyJob> findByEmpidAndJobid(String empid, String jobid);
 
 }
 
