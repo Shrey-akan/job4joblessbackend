2bc1df71857eb6225b1d9c7bbaad93624872c4f5
shrey_akan <shereyansjain0904@gmail.com>
2024-01-04 01:45:17 -0800

    lpu

--- src/main/java/com/demo/oragejobsite/entity/SendMessage.java
+++ src/main/java/com/demo/oragejobsite/entity/SendMessage.java
@@ -1,9 +1,13 @@
 package com.demo.oragejobsite.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 public class SendMessage {
@@ -14,42 +18,64 @@ public class SendMessage {
     private String messageTo;
     private String messageFrom;
     private String message;
+    
+    @CreationTimestamp
+    private Date sendTime;
+    
 	public SendMessage() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public SendMessage(Long messageId, String messageTo, String messageFrom, String message) {
+
+	public SendMessage(Long messageId, String messageTo, String messageFrom, String message, Date sendTime) {
 		super();
 		this.messageId = messageId;
 		this.messageTo = messageTo;
 		this.messageFrom = messageFrom;
 		this.message = message;
+		this.sendTime = sendTime;
 	}
+
 	public Long getMessageId() {
 		return messageId;
 	}
+
 	public void setMessageId(Long messageId) {
 		this.messageId = messageId;
 	}
+
 	public String getMessageTo() {
 		return messageTo;
 	}
+
 	public void setMessageTo(String messageTo) {
 		this.messageTo = messageTo;
 	}
+
 	public String getMessageFrom() {
 		return messageFrom;
 	}
+
 	public void setMessageFrom(String messageFrom) {
 		this.messageFrom = messageFrom;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-    
+
+	public Date getSendTime() {
+		return sendTime;
+	}
+
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
+
     
    
 }
