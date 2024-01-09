c287a48a3fc6605e6cac4a1fc7cb8c4880173a8d
shrey_akan <shereyansjain0904@gmail.com>
2024-01-08 19:53:32 -0800

    ng update 9 jan

--- src/main/java/com/demo/oragejobsite/socket/WebSocketController.java
+++ src/main/java/com/demo/oragejobsite/socket/WebSocketController.java
@@ -23,33 +23,33 @@ import com.demo.oragejobsite.service.MessageService;
 @CrossOrigin(origins = "https://job4jobless.com")
 public class WebSocketController {
 
-    private final SimpMessagingTemplate messagingTemplate;
-    private final MessageService messageService; // Inject the MessageService
-
-    @Autowired
-    public WebSocketController(SimpMessagingTemplate messagingTemplate, MessageService messageService) {
-        this.messagingTemplate = messagingTemplate;
-        this.messageService = messageService;
-    }
-
-    @MessageMapping("/send")
-    public void sendMessage(@Payload SendMessage message) {
-        // Save the message to the database (you can use your existing service)
-        SendMessage savedMessage = messageService.saveMessage(message);
-        
-        // Broadcast the message to all subscribers
-        messagingTemplate.convertAndSend("/topic/messages", savedMessage);
-    }
-
-    @GetMapping("/fetchMessages")
-    public ResponseEntity<List<SendMessage>> fetchMessages() {
-        try {
-            List<SendMessage> messages = messageService.getAllMessages();
-            return new ResponseEntity<>(messages, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-	
+//    private final SimpMessagingTemplate messagingTemplate;
+//    private final MessageService messageService; // Inject the MessageService
+//
+//    @Autowired
+//    public WebSocketController(SimpMessagingTemplate messagingTemplate, MessageService messageService) {
+//        this.messagingTemplate = messagingTemplate;
+//        this.messageService = messageService;
+//    }
+//
+//    @MessageMapping("/send")
+//    public void sendMessage(@Payload SendMessage message) {
+//        // Save the message to the database (you can use your existing service)
+//        SendMessage savedMessage = messageService.saveMessage(message);
+//        
+//        // Broadcast the message to all subscribers
+//        messagingTemplate.convertAndSend("/topic/messages", savedMessage);
+//    }
+//
+//    @GetMapping("/fetchMessages")
+//    public ResponseEntity<List<SendMessage>> fetchMessages() {
+//        try {
+//            List<SendMessage> messages = messageService.getAllMessages();
+//            return new ResponseEntity<>(messages, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//	
 }
 
