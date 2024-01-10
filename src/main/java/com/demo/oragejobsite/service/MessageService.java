82fffb63cfe82b320fad9d33bcbff4521e12ee83
shrey_akan <shereyansjain0904@gmail.com>
2024-01-10 00:37:55 -0800

    updatng service

--- src/main/java/com/demo/oragejobsite/service/MessageService.java
+++ src/main/java/com/demo/oragejobsite/service/MessageService.java
@@ -1,7 +1,34 @@
 package com.demo.oragejobsite.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.demo.oragejobsite.dao.MessageEntityRepository;
+import com.demo.oragejobsite.entity.ChatMessage;
+import com.demo.oragejobsite.entity.SendMessage;
+
+@Service
 public class MessageService {
 
+    @Autowired
+    private MessageEntityRepository messageEntityRepository;
 
+    public void saveMessage(ChatMessage chatMessage) {
+    	SendMessage messageEntity = convertToEntity(chatMessage);
+        messageEntityRepository.save(messageEntity);
+    }
+
+    public void saveJoinMessage(ChatMessage chatMessage) {
+        // Optionally, perform specific operations for join messages
+        saveMessage(chatMessage);
+    }
+
+    private SendMessage convertToEntity(ChatMessage chatMessage) {
+    	SendMessage messageEntity = new SendMessage();
+        messageEntity.setMessageTo(chatMessage.getMessageTo());
+        messageEntity.setMessageFrom(chatMessage.getSender());
+        messageEntity.setMessage(chatMessage.getContent());
+
+        return messageEntity;
+    }
 }
