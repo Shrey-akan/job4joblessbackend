4dfa941cc54ae1148e6743d67a2294efa2978616
shrey_akan <shereyansjain0904@gmail.com>
2024-01-23 13:47:18 +0530

    updating time in notification and applyjob

--- src/main/java/com/demo/oragejobsite/entity/ApplyJob.java
+++ src/main/java/com/demo/oragejobsite/entity/ApplyJob.java
@@ -1,7 +1,10 @@
 package com.demo.oragejobsite.entity;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Document(collection = "applyjob")
 public class ApplyJob {
@@ -29,19 +32,17 @@ public class ApplyJob {
 	private String uid;
 	private String jobid;
 	private String profileupdate;
-	
-	
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date sendTime = new Date();
 	public ApplyJob() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
 	public ApplyJob(String juid, String juname, String jumail, String jucompny, String jutitle, String juresume,
 			String jurelocation, String jueducation, String juexperience, String juexpinjava, String juexpjsp,
 			String juinterviewdate, String jujavavalid, String jujobtitle, String jucompanyname, String description,
 			Long juphone, String julastsal, String juexpecsalary, String empid, String uid, String jobid,
-			String profileupdate) {
+			String profileupdate, Date sendTime) {
 		super();
 		this.juid = juid;
 		this.juname = juname;
@@ -66,238 +67,152 @@ public class ApplyJob {
 		this.uid = uid;
 		this.jobid = jobid;
 		this.profileupdate = profileupdate;
+		this.sendTime = sendTime;
 	}
-
-
 	public String getJuid() {
 		return juid;
 	}
-
-
 	public void setJuid(String juid) {
 		this.juid = juid;
 	}
-
-
 	public String getJuname() {
 		return juname;
 	}
-
-
 	public void setJuname(String juname) {
 		this.juname = juname;
 	}
-
-
 	public String getJumail() {
 		return jumail;
 	}
-
-
 	public void setJumail(String jumail) {
 		this.jumail = jumail;
 	}
-
-
 	public String getJucompny() {
 		return jucompny;
 	}
-
-
 	public void setJucompny(String jucompny) {
 		this.jucompny = jucompny;
 	}
-
-
 	public String getJutitle() {
 		return jutitle;
 	}
-
-
 	public void setJutitle(String jutitle) {
 		this.jutitle = jutitle;
 	}
-
-
 	public String getJuresume() {
 		return juresume;
 	}
-
-
 	public void setJuresume(String juresume) {
 		this.juresume = juresume;
 	}
-
-
 	public String getJurelocation() {
 		return jurelocation;
 	}
-
-
 	public void setJurelocation(String jurelocation) {
 		this.jurelocation = jurelocation;
 	}
-
-
 	public String getJueducation() {
 		return jueducation;
 	}
-
-
 	public void setJueducation(String jueducation) {
 		this.jueducation = jueducation;
 	}
-
-
 	public String getJuexperience() {
 		return juexperience;
 	}
-
-
 	public void setJuexperience(String juexperience) {
 		this.juexperience = juexperience;
 	}
-
-
 	public String getJuexpinjava() {
 		return juexpinjava;
 	}
-
-
 	public void setJuexpinjava(String juexpinjava) {
 		this.juexpinjava = juexpinjava;
 	}
-
-
 	public String getJuexpjsp() {
 		return juexpjsp;
 	}
-
-
 	public void setJuexpjsp(String juexpjsp) {
 		this.juexpjsp = juexpjsp;
 	}
-
-
 	public String getJuinterviewdate() {
 		return juinterviewdate;
 	}
-
-
 	public void setJuinterviewdate(String juinterviewdate) {
 		this.juinterviewdate = juinterviewdate;
 	}
-
-
 	public String getJujavavalid() {
 		return jujavavalid;
 	}
-
-
 	public void setJujavavalid(String jujavavalid) {
 		this.jujavavalid = jujavavalid;
 	}
-
-
 	public String getJujobtitle() {
 		return jujobtitle;
 	}
-
-
 	public void setJujobtitle(String jujobtitle) {
 		this.jujobtitle = jujobtitle;
 	}
-
-
 	public String getJucompanyname() {
 		return jucompanyname;
 	}
-
-
 	public void setJucompanyname(String jucompanyname) {
 		this.jucompanyname = jucompanyname;
 	}
-
-
 	public String getDescription() {
 		return description;
 	}
-
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-
 	public Long getJuphone() {
 		return juphone;
 	}
-
-
 	public void setJuphone(Long juphone) {
 		this.juphone = juphone;
 	}
-
-
 	public String getJulastsal() {
 		return julastsal;
 	}
-
-
 	public void setJulastsal(String julastsal) {
 		this.julastsal = julastsal;
 	}
-
-
 	public String getJuexpecsalary() {
 		return juexpecsalary;
 	}
-
-
 	public void setJuexpecsalary(String juexpecsalary) {
 		this.juexpecsalary = juexpecsalary;
 	}
-
-
 	public String getEmpid() {
 		return empid;
 	}
-
-
 	public void setEmpid(String empid) {
 		this.empid = empid;
 	}
-
-
 	public String getUid() {
 		return uid;
 	}
-
-
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
-
-
 	public String getJobid() {
 		return jobid;
 	}
-
-
 	public void setJobid(String jobid) {
 		this.jobid = jobid;
 	}
-
-
 	public String getProfileupdate() {
 		return profileupdate;
 	}
-
-
 	public void setProfileupdate(String profileupdate) {
 		this.profileupdate = profileupdate;
 	}
-	
+	public Date getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
 	
 	
 	
--- src/main/java/com/demo/oragejobsite/entity/Notification.java
+++ src/main/java/com/demo/oragejobsite/entity/Notification.java
@@ -1,7 +1,10 @@
 package com.demo.oragejobsite.entity;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Document(collection = "notification")
 public class Notification {
@@ -12,21 +15,14 @@ public class Notification {
 	private String ndescription;
 	private String notisend;
 	private String notifyuid;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date sendTime = new Date();
 	public Notification() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-
-
-
-
-
-
-
-	
 	public Notification(String nid, String nhead, String nsubhead, String ndescription, String notisend,
-			String notifyuid) {
+			String notifyuid, Date sendTime) {
 		super();
 		this.nid = nid;
 		this.nhead = nhead;
@@ -34,34 +30,14 @@ public class Notification {
 		this.ndescription = ndescription;
 		this.notisend = notisend;
 		this.notifyuid = notifyuid;
+		this.sendTime = sendTime;
 	}
-
-
-
-
-
-
-
-
-
 	public String getNid() {
 		return nid;
 	}
-
-
-
-
-
-
 	public void setNid(String nid) {
 		this.nid = nid;
 	}
-
-
-
-
-
-
 	public String getNhead() {
 		return nhead;
 	}
@@ -80,40 +56,26 @@ public class Notification {
 	public void setNdescription(String ndescription) {
 		this.ndescription = ndescription;
 	}
-
 	public String getNotisend() {
 		return notisend;
 	}
-
 	public void setNotisend(String notisend) {
 		this.notisend = notisend;
 	}
-
-
-
-
-
-
-
-
-
 	public String getNotifyuid() {
 		return notifyuid;
 	}
-
-
-
-
-
-
-
-
-
 	public void setNotifyuid(String notifyuid) {
 		this.notifyuid = notifyuid;
 	}
-
-
+	public Date getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
 	
+    
+    
 }
 
