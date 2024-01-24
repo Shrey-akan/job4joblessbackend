5532997c1f5de537a786fbf48bf270a7bede2cc9
shrey_akan <shereyansjain0904@gmail.com>
2024-01-24 13:47:32 +0530

    update at 24

--- src/main/java/com/demo/oragejobsite/entity/PostJob.java
+++ src/main/java/com/demo/oragejobsite/entity/PostJob.java
@@ -12,6 +12,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class PostJob {
 @Id
 private String jobid; 
+private String name;
+private String email;
 private String jobtitle;
 private String companyforthisjob;
 private Long numberofopening;
@@ -34,11 +36,13 @@ public PostJob() {
 	super();
 }
 
-public PostJob(String jobid, String jobtitle, String companyforthisjob, Long numberofopening, String locationjob,
-		String jobtype, String schedulejob, Long payjob, Long payjobsup, String descriptiondata, String empid,
-		Date sendTime, String uid, Boolean status) {
+public PostJob(String jobid, String name, String email, String jobtitle, String companyforthisjob, Long numberofopening,
+		String locationjob, String jobtype, String schedulejob, Long payjob, Long payjobsup, String descriptiondata,
+		String empid, Date sendTime, String uid, Boolean status) {
 	super();
 	this.jobid = jobid;
+	this.name = name;
+	this.email = email;
 	this.jobtitle = jobtitle;
 	this.companyforthisjob = companyforthisjob;
 	this.numberofopening = numberofopening;
@@ -49,207 +53,138 @@ public PostJob(String jobid, String jobtitle, String companyforthisjob, Long num
 	this.payjobsup = payjobsup;
 	this.descriptiondata = descriptiondata;
 	this.empid = empid;
-	this.sendTime = new Date();
+	this.sendTime = sendTime;
 	this.uid = uid;
 	this.status = status;
 }
-
-
-
 
 public String getJobid() {
 	return jobid;
 }
 
-
-
-
 public void setJobid(String jobid) {
 	this.jobid = jobid;
 }
 
+public String getName() {
+	return name;
+}
 
+public void setName(String name) {
+	this.name = name;
+}
 
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
 
 public String getJobtitle() {
 	return jobtitle;
 }
 
-
-
-
 public void setJobtitle(String jobtitle) {
 	this.jobtitle = jobtitle;
 }
-
-
-
 
 public String getCompanyforthisjob() {
 	return companyforthisjob;
 }
 
-
-
-
 public void setCompanyforthisjob(String companyforthisjob) {
 	this.companyforthisjob = companyforthisjob;
 }
-
-
-
 
 public Long getNumberofopening() {
 	return numberofopening;
 }
 
-
-
-
 public void setNumberofopening(Long numberofopening) {
 	this.numberofopening = numberofopening;
 }
-
-
-
 
 public String getLocationjob() {
 	return locationjob;
 }
 
-
-
-
 public void setLocationjob(String locationjob) {
 	this.locationjob = locationjob;
 }
-
-
-
 
 public String getJobtype() {
 	return jobtype;
 }
 
-
-
-
 public void setJobtype(String jobtype) {
 	this.jobtype = jobtype;
 }
-
-
-
 
 public String getSchedulejob() {
 	return schedulejob;
 }
 
-
-
-
 public void setSchedulejob(String schedulejob) {
 	this.schedulejob = schedulejob;
 }
-
-
-
 
 public Long getPayjob() {
 	return payjob;
 }
 
-
-
-
 public void setPayjob(Long payjob) {
 	this.payjob = payjob;
 }
-
-
-
 
 public Long getPayjobsup() {
 	return payjobsup;
 }
 
-
-
-
 public void setPayjobsup(Long payjobsup) {
 	this.payjobsup = payjobsup;
 }
-
-
-
 
 public String getDescriptiondata() {
 	return descriptiondata;
 }
 
-
-
-
 public void setDescriptiondata(String descriptiondata) {
 	this.descriptiondata = descriptiondata;
 }
-
-
-
 
 public String getEmpid() {
 	return empid;
 }
 
-
-
-
 public void setEmpid(String empid) {
 	this.empid = empid;
 }
-
-
-
 
 public Date getSendTime() {
 	return sendTime;
 }
 
-
-
-
 public void setSendTime(Date sendTime) {
 	this.sendTime = sendTime;
 }
-
-
-
 
 public String getUid() {
 	return uid;
 }
 
-
-
-
 public void setUid(String uid) {
 	this.uid = uid;
 }
-
-
-
 
 public Boolean getStatus() {
 	return status;
 }
 
-
-
-
 public void setStatus(Boolean status) {
 	this.status = status;
 }
-
 
 
 
